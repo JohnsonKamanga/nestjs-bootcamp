c796030594e470fa3e6a8db193e3a6546a00cb29
JohnsonKamanga <johnsonkamanga2@gmail.com>
2026-04-06 09:25:46 +0200

    feat: updated products controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,17 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductsService } from './products.service';
+
 
 @Controller('products')
 export class ProductsController {
-    constructor(private readonly productsService : ProductsService){}
+  constructor(private readonly productsService: ProductsService) {}
 
-    @Get(":id")
-    findOne(id: number) {
-        return this.productsService.findOne(id);
-    }
 
-    @Get()
-    findAll() {
-        return this.productsService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
+
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.productsService.findOne(+id);
+  }
+
+
+  @Post()
+  create(@Body() body: any) {
+    return this.productsService.create(body);
+  }
+
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.productsService.update(+id, body);
+  }
+
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.productsService.remove(+id);
+  }
 }
